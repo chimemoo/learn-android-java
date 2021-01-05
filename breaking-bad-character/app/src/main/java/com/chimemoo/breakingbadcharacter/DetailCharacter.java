cb46febefd3a4ebf3c1de1d6cc049235356cf4b9
chimemoo <christ.16062@student.unsika.ac.id>
2021-01-05 11:17:18 +0700

    add detail character on breaking bad character app

---
+++ breaking-bad-character/app/src/main/java/com/chimemoo/breakingbadcharacter/DetailCharacter.java
@@ -0,0 +1,14 @@
+package com.chimemoo.breakingbadcharacter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailCharacter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_character);
+    }
+}
